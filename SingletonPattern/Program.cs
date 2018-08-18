4d4b82edf37ecdbcb008e1560b79b02e05e48650
Daniel Peña <digrape07@gmail.com>
2018-08-18 16:47:49 -0400

    Singleton Pattern

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string singletonClass = "ServicesEx";
+            Console.WriteLine("----- Singleton Pattern Implementation -----");
+            Console.WriteLine($"Singleton class: {singletonClass}");
+            Console.WriteLine("");
+
+            ServiceEx singleton1 = ServiceEx.Instance;
+            ServiceEx singleton2 = ServiceEx.Instance;
+
+            if(singleton1 == singleton2)
+            {
+                Console.WriteLine("Singleton Pattern Implemented Successfully!");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ SingletonPattern/ServiceEx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    // Note: An instance of this class is ONLY created when any of the static methods
+    //       are called.
+
+    // sealed keyword: Prevents inheritance.
+    public sealed class ServiceEx
+    {
+        private static  ServiceEx instance = null; // Class instance
+        private static int numberOfInstances = 0; // Quanity of instances created
+        private static readonly Object teastableObj = new object(); // For lock control
+
+        // Private constructor: Prevents instantiation out of this class.
+        private ServiceEx() {
+            numberOfInstances++;
+        }
+
+        public static ServiceEx Instance { //Getting the instance.
+
+            get {
+                if(instance == null)
+                {
+                    lock (teastableObj) // Allow access to only ONE thread at a time.
+                    {
+                        instance = new ServiceEx(); // Creates an instance of this class
+                    }
+                }
+                return instance; // return the instance
+            }
+        }
+    }
+}
